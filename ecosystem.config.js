8c19df19f7252a47f3eb10e4b299ce7d6582f932
nemokoala <fdxguy2@gmail.com>
2025-10-27 00:37:16 +0900

    fix: cwd 경로 변경

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: "imagen-app",
-      cwd: "/home/koala/imagen",
+      cwd: process.env.APP_CWD || "/home/koala/imagen", // ← 기본값은 유지 (fallback)
       script: "node",
       args: ".next/standalone/server.js",
       env_production: {

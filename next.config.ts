9977dcbe99c1c3604a4dbc4cfea69e00400ed2d7
nemokoala <fdxguy2@gmail.com>
2025-10-26 21:09:42 +0900

    fix: 패키지 충돌 해결, nextjs, react 신버젼 업데이트 및 리액트 컴파일러 적용

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  compiler: {
+    reactCompiler: true,
+  },
 };
 
 export default nextConfig;

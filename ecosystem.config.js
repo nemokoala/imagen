c88abecf82f89a853ba72c83ff80eca945283655
nemokoala <fdxguy2@gmail.com>
2026-02-08 23:11:14 +0900

    fix: script 수정

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,11 +3,11 @@ module.exports = {
     {
       name: "imagen-app",
       cwd: process.env.APP_CWD || "/home/koala/imagen", // ← 기본값은 유지 (fallback)
-      script: "node",
-      instances: "2",
+      script: ".next/standalone/server.js",
+      instances: "max",
       exec_mode: "cluster",
       kill_timeout: 3000,
-      args: ".next/standalone/server.js",
+      wait_ready: true,
       env_production: {
         NODE_ENV: "production",
         PORT: "3000",

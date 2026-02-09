96b4dc466bd4264ee3d8b6cf798e6f900f4a118d
nemokoala <fdxguy2@gmail.com>
2026-02-09 23:42:12 +0900

    fix: next-pwa 제거 및 turbopack 으로 변경

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,4 @@
 import type { NextConfig } from "next";
-import withPWA from "@ducanh2912/next-pwa";
 
 const nextConfig: NextConfig = {
   reactCompiler: true,
@@ -21,15 +20,4 @@ const nextConfig: NextConfig = {
   },
 };
 
-const pwaConfig = withPWA({
-  dest: "public",
-  cacheOnFrontEndNav: true,
-  aggressiveFrontEndNavCaching: true,
-  reloadOnOnline: true,
-  disable: process.env.NODE_ENV === "development",
-  workboxOptions: {
-    disableDevLogs: true,
-  },
-});
-
-export default pwaConfig(nextConfig);
+export default nextConfig;
